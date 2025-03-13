8a2c731d05b5587e2c20b74e5ba2365564ccf2ad
Pablo <oscar282004@gmail.com>
2025-03-13 10:09:21 +0100

    Endpoint MatchHockey
    Création de l'endpoint pour les matchs
    Ajout d'une fonction deliverResponse dans controllers
    Modif de l'endpoint de Joueur pour utiliser deliverResponse

---
+++ controllers/deliverResponse.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Fonction qui va envoyer la réponse à l'utilisateur
+ */
+function deliverResponse($status_code, $status_message, $data = null)
+{
+    // Paramétrage de l'entête HTTP
+    header("HTTP/1.1 $status_code $status_message");
+    $response['status'] = $status_code;
+    $response['status_message'] = $status_message;
+    $response['data'] = $data;
+
+    // Envoi de la réponse au format JSON
+    $json_response = json_encode($response);
+    echo $json_response;
+}
