4595d98bff84c3c92e1aaf0fa9d469ffd226ecae
FloSho <florian.lepronguillout@gmail.com>
2025-03-19 10:44:55 +0000

    Fin endpoint Stats

--- src/StatsEndpoint.php
+++ src/StatsEndpoint.php
@@ -36,45 +36,6 @@ switch ($http_methode) {
             deliverResponse(201, 'Succes', $data); //on envoie la réponse
         }
         break;
-
-    case 'POST':
-        // Récupération des données dans le corps
-        $postedData = file_get_contents('php://input');
-        $data = json_decode($postedData, true); //Reçoit du json et renvoi une adaptation exploitable en php. Le paramètre true impose un tableau en retour et non un objet.
-        $datareponse = $joueur->ajouterJoueur($data);
-
-        if ($datareponse == null) {
-            deliverResponse(500, 'Erreur de synstaxe sans votre requête ou erreur serveur lors de la création (vérifier bien l\'orthographe de votre requête)');
-        } else {
-            deliverResponse(201, 'Créer avec succès', $datareponse);
-        }
-        break;
-    case 'PUT':
-        $postedData = file_get_contents('php://input');
-        $data = json_decode($postedData, true);
-        $id = htmlspecialchars($_GET['id']); //Si l'on veut récupérer l'id de l'url on doit passer par $_GET mm si on est en PUT ou autres requêtes
-
-        $dataReponse = $joueur->modifierJoueur($id, $data);
-        if ($dataReponse == null) {
-            deliverResponse(500, 'Erreur de synstaxe sans votre requête ou erreur serveur lors de la modification (vérifier bien l\'orthographe de votre requête)');
-        } elseif ($dataReponse === 'ID non trouvé') {
-            deliverResponse(404, 'ID non trouvé');
-        } else {
-            deliverResponse(200, 'Données modifiées avec succès');
-        }
-        break;
-    case 'DELETE' :
-        $id = htmlspecialchars($_GET['id']);
-        $dataReponse = $joueur->supprimerJoueur($id);
-        if ($dataReponse == 'ok') {
-            deliverResponse(200, 'Joeur supprimé avec succès');
-        } elseif ($dataReponse === 'ID non trouvé') {
-            deliverResponse(404, 'ID non trouvé');
-        } else {
-            deliverResponse(500, 'Erreur lors de la suppression');
-        }
-        break;
-
 }
 exit();
 
